5e3f0b4ceda6ed04e526fdab26ea4b23a10a5902
wangjiannan <904089747@qq.com>
2017-03-04 16:51:36 +0800

    add json

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,16 +2,33 @@ import React from 'react';
 import axios from 'axios'
 
 class Blog extends React.Component {
-  componentDidMount(){
-    axios.get('https://raw.githubusercontent.com/newming/demodata/master/duopingshidai.json')
-    .then(res=>console.log(res))
+  constructor(){
+    super();
+    this.state={
+      data:[]
+    }
   }
-  render () {
+  componentDidMount(){
+    axios.get('https://raw.githubusercontent.com/arronwjn/myblog/master/blogs/blogs.json')
+    .then(res=>this.setState({data:res.data}))
+    // .then(res=>console.log(res))
+  }
+  render(){
     return(
-      <div>
-        <div>
-          blog
-        </div>
+      <div style={{width:'100%'}}>
+          {this.state.data.length==0?'请稍后':
+            this.state.data.map((item,index)=>(
+              <div key={index} className='blog-card'>
+                <div className='blog-index'><span>{index+1}</span></div>
+                <div className='blog-desc'>
+                  <h3>{item.title}</h3>
+                  <p>{item.desc}</p>
+                  <a href={item.url}>阅读更多</a>
+                  <span>{item.data}</span>
+                </div>
+              </div>
+            ))
+          }
       </div>
     )
   }
