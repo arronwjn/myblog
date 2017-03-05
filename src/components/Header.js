e436bfbdc8e5171f9e3a6b995c11d4826e58f16e
wangjiannan <904089747@qq.com>
2017-03-05 10:39:03 +0800

    add md

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {browserHistory} from 'react-router'
+import {hashHistory} from 'react-router'
 
 class Header extends React.Component {
 
   render () {
     return(
       <header>
-        <button onClick={()=>browserHistory.go(-1)}><i className="fa fa-arrow-left" aria-hidden="true"></i>Back</button>
+        <button onClick={()=>hashHistory.go(-1)}><i className="fa fa-arrow-left" aria-hidden="true"></i>Back</button>
         <h3>{this.props.title}</h3>
-        <button onClick={()=>browserHistory.push('/')}><i className="fa fa-home" aria-hidden="true"></i>Home</button>
+        <button onClick={()=>hashHistory.push('/')}><i className="fa fa-home" aria-hidden="true"></i>Home</button>
       </header>
     )
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import {Link} from 'react-router';
-import marked from 'marked'；
-import 'main.css'
+import marked from 'marked';
+import '../main.css'
 
 
 marked.setOptions({
