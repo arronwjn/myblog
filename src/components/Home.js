67d0a303bd2cfae19cdfe51f5a598153d2479a79
wangjiannan <904089747@qq.com>
2017-03-05 15:18:15 +0800

    modify

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,13 @@ class Home extends React.Component {
     return(
       <div className='home'>
         <div className='home-cover'>
-            <h1>I'M WangJianNan</h1>
-            <p>FrontEnd Web Developer</p>
-            <a href='https://github.com/arronwjn'><i className="fa fa-github" aria-hidden="true"></i>&nbsp;My GitHub</a>
+            <div>
+              <h1>I'M WangJianNan</h1>
+              <p>FrontEnd Web Developer</p>
+              <a href='https://github.com/arronwjn'><i className="fa fa-github" aria-hidden="true"></i>&nbsp;My GitHub</a>
+            </div>
         </div>
-        
+
       </div>
     )
   }
