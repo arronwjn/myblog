fc89c5e6662bcdc7401122921fb2443bfa9f76fd
wangjiannan <904089747@qq.com>
2017-03-05 14:34:37 +0800

    add

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Routers from './routes'
-import App from './App.js';
-import './main.css';
-import 'highlight.js/styles/docco.css'
-
-render(<Routers />,document.getElementById('root'));
