1a176f0f4b6e784267b89585623ecca745074ba4
wangjiannan <904089747@qq.com>
2017-03-05 14:10:39 +0800

    checkout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LeftNav from './components/LeftNav';
 
 
 class App extends React.Component {
   constructor(){
     super();
     this.state={
-      title:"Personal Website"
+      title:"Personal Website",
+      showLeftNav:false
     }
   }
   handleClick(e){
@@ -15,18 +17,38 @@ class App extends React.Component {
       // this.state.title=e;
       // this.setState({title:this.state.title})
   }
+  setTitle(){
+    this.setState({
+      title:this.props.router.isActive('/',true)?'Personal Website':
+      this.props.router.isActive('/blog')?'博客':
+      this.props.router.isActive('/work')?'工作':
+      this.props.router.isActive('/note')?'笔记':'文章'
+    })
+  }
+  componentWillReceiveProps(){
+    this.setTitle();
+  }
   componentWillMount(){
-
-
+    this.setLeftNav()
+    window.onresize=this.setLeftNav.bind(this)
+    this.setTitle();
+  }
+  setLeftNav(){
+    if(window.innerWidth>750){
+      this.setState({showLeftNav:true})
+    }else{
+      this.setState({showLeftNav:false})
+    }
   }
   render () {
+    console.log(this.props)
     return(
       <div className='root'>
-        <Header title={this.state.title}/>
+        {this.state.showLeftNav?<LeftNav title={this.state.title}/>:<Header title={this.state.title}/>}
         <div className='mywrap'>
           {this.props.children}
         </div>
-        <Footer handleClick={this.handleClick.bind(this)}/>
+        {this.state.showLeftNav?null:<Footer handleClick={this.handleClick.bind(this)}/>}
       </div>
     )
   }
